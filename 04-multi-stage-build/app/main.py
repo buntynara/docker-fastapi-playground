4aec69d81e3f61015044a2feadbfa3c3c3d034f0
Bunty Nara <1707821+buntynara@users.noreply.github.com>
2026-08-22 18:48:48 +0530

    style: format code

--- 04-multi-stage-build/app/main.py
+++ 04-multi-stage-build/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.core.database import lifespan
 from app.api.routes import router
-app=FastAPI(title='Docker FastAPI Playground',description='Project 04 - Multi-Stage Build',version='1.0.0',lifespan=lifespan)
+
+app = FastAPI(
+    title = 'Docker FastAPI Playground',
+    description = 'Project 04 - Multi-Stage Build',
+    version = '1.0.0',
+    lifespan = lifespan
+)
+
 app.include_router(router)
